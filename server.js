2fb47b9a6022c02f348cbd72f0e7413c585bd5fb
bkauf <bkauf@bkaufs-MacBook-Pro.local>
2017-01-10 15:03:30 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+
+	var os = require("os");
+   var hostname = os.hostname();
+  res.send('Hello from host:<h1 style="color:yellow;background:black">'+hostname+'</h1>');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
