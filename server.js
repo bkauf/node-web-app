e7c94a850504a5be35b664488fb9b2440f8aed72
Brian Kaufman <bkauf@Brians-MacBook-Pro.local>
2017-02-06 18:12:48 -0500

    text change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
    var os = require("os");
    var hostname = os.hostname();
    var html     = '<div style="width:100%;"><div style="width:300px;margin-left:auto; margin-right:auto;">';
-       html    += 'Hello from host: <h1 style="color:yellow;background:black"> '+hostname+'</h1></div></div>'; 
+       html    += 'Helloo from host: <h1 style="color:yellow;background:black"> '+hostname+'</h1></div></div>'; 
    res.send(html);
 });
 
