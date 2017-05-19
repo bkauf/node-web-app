951119a45fb2e2735ca8600990c035145901c9df
Brian Kaufman <bkauf@Brians-MacBook-Pro.local>
2017-05-19 15:29:13 -0400

    secrets values

--- routes/read.js
+++ routes/read.js
@@ -5,9 +5,14 @@ var fs = require('fs');
 /* GET file contents. */
 router.post('/', function(req, res, next) {
 
+  const path    = '/run/secrets/';
+  var file_name = req.param('file_name');
+  var file      = file_name.split("#");
 
-  res.send('test');
-
+      fs.readFile(path+file, 'utf8', function(err, data) {
+          if (err) throw err;
+          res.send(data);
+      });
 
 });
 module.exports = router;
